a8f708e8b8af1d779b51e96a3ee4f1498f4f8fe8
Zxphish <127034533+Zxphish@users.noreply.github.com>
2023-03-05 20:10:47 +0530

    Add files via upload

---
+++ Rxcehack.py
@@ -0,0 +1,187 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from os import system, name
+import itertools
+import threading
+import time
+import sys
+import datetime
+from base64 import b64decode,b64encode
+from datetime import date
+expirydate = datetime.date(2023, 11, 1)
+#expirydate = datetime.date(2022, 8, 30)
+today=date.today()
+green="\033"
+neon="\033"
+nc="\033"
+red="\033"
+purple="\033"
+yellow="\033"
+voilet="\033"
+def hero():
+    def chalo():
+        done = False
+        #here is the animation
+        def animate():
+            for c in itertools.cycle(['|', '/', '-', '\\']) :
+                if done:
+                    break
+                sys.stdout.write('\rhacking in the Bcone server for next colour--------- ' + c)
+                sys.stdout.flush()
+                time.sleep(0.1)
+            sys.stdout.write('\rDone!     ')
+        t = threading.Thread(target=animate)
+        t.start()
+        time.sleep(0.1)
+        done = True
+    def chalo1():
+        done = False
+        #here is the animation
+        def animate():
+            for c in itertools.cycle(['|', '/', '-', '\\']):
+                if done:
+                    break
+                sys.stdout.write('\rgetting the colour wait --------- ' + c)
+                sys.stdout.flush()
+                time.sleep(0.1)
+            sys.stdout.write('\rDone!     ')
+        t = threading.Thread(target=animate)
+        t.start()
+        #long process here
+        time.sleep(20)
+        done = True
+    def clear():
+        # for windows
+        if name == 'nt':
+            _ = system('cls')
+        # for mac and linux(here, os.name is 'posix')
+        else:
+            _ = system('clear')
+    clear()
+    y=1
+    newperiod=period
+    banner='mantrimall'
+    thisway=[2,6,8,11,12,15,16,18,19,20]
+    thatway=[1,3,4,5,7,9,10,14,13,17]
+    numbers=[]
+    i=1
+    while(y):
+        clear()
+        system(banner)
+        print(f"{red}mantrimall.in)
+        print("Enter ",newperiod," Bcone Price :")
+        current=input()
+        current=int(current)
+        chalo()
+        print("\n---------Successfully hacked the server-----------")
+        chalo1()
+        print("\n---------Successfully got the colour -------------")
+        print('\n')
+        last2=str(current)[-2:]
+        #samjha_maadarchod=lawde_time_period
+        if(newperiod%2==0):
+                sum=getSum(current)
+                if(sum%2==0):
+                       print(newperiod+1," : RED")
+                else:
+                       print(newperiod+1," : GREEN")
+   else:
+                sum=getSum(current)
+                if(sum%2==0):
+                       print(newperiod+1,": RED")
+                else:
+                       print(newperiod+1,": GREEN")
+        newperiod+=1
+        numbers.append(current)
+        y=input("Do you want to play : Press 1 and 0 to exit \n")
+        if(y==0):
+            y=False
+        if (len(numbers)>31):
+            clear()
+            system('figlet Thank youðŸ¤‘!!')
+            print("Play on next specified timeðŸ¤‘!!")
+            print("-----------â±ï¸Current Time UPâ±ï¸----------")
+            sys.exit(" \n \n \n Contact on Telegram @HACK_RXCE")
+            #print(numbers)
+  
+
+
+
+if(expirydate>today):
+    now = datetime.datetime.now()
+    First = now.replace(hour=13, minute=55, second=0, microsecond=0)
+    Firstend = now.replace(hour=14, minute=35, second=0, microsecond=0)
+    Second = now.replace(hour=15, minute=55, second=0, microsecond=0)
+    Secondend = now.replace(hour=16, minute=35, second=0, microsecond=0)
+    Third = now.replace(hour=16, minute=55, second=0, microsecond=0)
+    Thirdend = now.replace(hour=17, minute=35, second=0, microsecond=0)
+    Final = now.replace(hour=17, minute=55, second=0, microsecond=0)
+    Finalend = now.replace(hour=18, minute=35, second=0, microsecond=0)
+
+    if (True):
+            period=420
+            hero()
+    elif(now>First and now<Firstend):
+            period=280
+            hero()
+    elif(now>Third and now<Thirdend):
+            period=372
+            hero()
+    elif(False):
+            period=230
+            hero()
+    else:
+        banner='mantrimall.in'
+        system(banner)
+        #print(f"{yellow}"Hi !! Thanks for buying the hack")
+        print("Hi ! thanks for trying our hack")
+        print("----------your play time----------")
+        #print("31st Aug 2023, 11:00 AM- 11:30 AM")
+        #print("31st Aug 2023, 02:00 PM- 02:30 PM")
+        print("23rd Sept 2023, 04:00 PM- 04:30 PM")
+        #print("31st Aug 2023, 08:00 PM- 08:30 PM")
+        print("Please play on the given time, and ")
+        print("If you think it is an error contact")
+        print(" admin on telegram @tusharma3 ")
+        else:
+        	def clear():
+        		#for windows
+        		if name == 'nt' :
+        			_ = system('cls')
+        		#for mac and linux(here, os.name is 'posix')
+        		else:
+        			 _ system('clear')
+            code="MZXCD"
+            code1="BSDF3"
+            code2="ASA6"
+            test="SD3"
+            night="N13"
+            nextday="DSDSXS"
+            banner="mantrimall.in"
+            rava=0
+            now=datetime.datetime.now()
+            Second=now.replace(hour=0, minute=55,)
+            Secondend=now.replace(hour=23. minute=)
+            Third=now.replace(hour=15, minute=)
+            Thirdend=now.replace(hour=18, minute=)
+            Final=now.replace(hour=18, minute=)
+            Finalend=now.replace(hour=22, minute=)
+            if (now>Second and now<Secondend):
+            	rava=290
+            elif(now>Third and now<Thirdend):
+            	        rava=350
+            elif(now>Final and now<Finalend) :
+            	        rava=410
+            system(banner)
+            print(f"{neon}*--------*--------*--------)
+            print("your hack has expired--- please contact")
+            print("on telegram ----@tusharam3")
+            print("      plan amount")
+            	        
+            	
+            	
+            	
+            	
+            	
+            	
+            	
